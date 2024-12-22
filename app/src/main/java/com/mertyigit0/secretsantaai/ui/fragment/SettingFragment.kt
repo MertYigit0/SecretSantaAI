7c0e4f5c4be5d8ab38d762f5ecf19d12284e0e4d
merty <mertyigit148@gmail.com>
2024-12-23 00:47:36 +0300

    Fragments created.

---
+++ app/src/main/java/com/mertyigit0/secretsantaai/ui/fragment/SettingFragment.kt
@@ -0,0 +1,28 @@
+package com.mertyigit0.secretsantaai.ui.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mertyigit0.secretsantaai.R
+
+class SettingFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_setting, container, false)
+    }
+
+
+}
